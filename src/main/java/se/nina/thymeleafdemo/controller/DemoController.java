de73189bdb7f8de39c88fb80693ece544bf54b17
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-02-28 18:00:50 +0100

    Initial commit

---
+++ src/main/java/se/nina/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package se.nina.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    //Create a mapping for "/hello"
+
+    @GetMapping("/hello")
+    public String sayHello(Model theModel) {
+        theModel.addAttribute("the date", new java.util.Date());
+    return "helloworld";
+    }
+
+}

d9b8a499cbbcf7b32818c38407716341aa9f2f66
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-03-02 20:04:35 +0100

    added add and form and redirection to directory

--- src/main/java/se/nina/thymeleafdemo/controller/VillagerController.java
+++ src/main/java/se/nina/thymeleafdemo/controller/VillagerController.java
@@ -3,6 +3,8 @@ package se.nina.thymeleafdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import se.nina.thymeleafdemo.entity.Villager;
 import se.nina.thymeleafdemo.service.VillagerService;
@@ -32,7 +34,26 @@ public class VillagerController {
         //add to the spring model
         theModel.addAttribute("villagers", theVillagers);
 
-        return "list-villagers";
+        return "villagers/list-villagers";
     }
 
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+
+        //create model attribute to bind form data
+        Villager theVillager = new Villager();
+
+        theModel.addAttribute("villager", theVillager);
+
+        return "villagers/villager-form";
+    }
+
+    @PostMapping("/save")
+    public String saveVillager(@ModelAttribute("villager") Villager theVillager) {
+        //save the villager
+        villagerService.save(theVillager);
+
+        //use a redirect to prevent duplicate submissions
+        return "redirect:/villagers/list";
+    }
 }

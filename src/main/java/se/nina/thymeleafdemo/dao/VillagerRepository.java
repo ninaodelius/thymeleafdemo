5fb6350015a8b7f9e9da972c86f0e99217779e3d
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-03-02 20:07:22 +0100

    added spring data jpa query in repo

--- src/main/java/se/nina/thymeleafdemo/dao/VillagerRepository.java
+++ src/main/java/se/nina/thymeleafdemo/dao/VillagerRepository.java
@@ -3,8 +3,15 @@ package se.nina.thymeleafdemo.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.nina.thymeleafdemo.entity.Villager;
 
+import java.util.List;
+
 //@RepositoryRestResource(path="residents")
 
 public interface VillagerRepository  extends JpaRepository <Villager, Integer> {
     //no need to write code. get CRUD methods for free.
+
+    //add a method to sort by name
+    //spring data jpa will parse the method name, find all by, orderby,name,
+    //looks for specific format and pattern and creates appropriate query
+    public List<Villager> findAllByOrderByName();
 }

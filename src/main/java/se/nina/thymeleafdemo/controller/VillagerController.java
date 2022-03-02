6d0ff6c2d8daa95bfe641f276cfe2a7043d116ae
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-03-02 18:56:00 +0100

    integrated db to thymeleaf app

--- src/main/java/se/nina/thymeleafdemo/controller/VillagerController.java
+++ src/main/java/se/nina/thymeleafdemo/controller/VillagerController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import se.nina.thymeleafdemo.entity.Villager;
+import se.nina.thymeleafdemo.service.VillagerService;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -14,12 +15,19 @@ import java.util.List;
 @RequestMapping("/villagers")
 public class VillagerController {
 
+    private VillagerService villagerService;
 
+    public VillagerController(VillagerService theVillagerService) {
+        villagerService = theVillagerService;
+    }
 
 
     //add mapping for "/list"
     @GetMapping("/list")
     public String listVillagers(Model theModel) {
+
+        //get villagers from db
+        List<Villager> theVillagers = villagerService.findAll();
 
         //add to the spring model
         theModel.addAttribute("villagers", theVillagers);
